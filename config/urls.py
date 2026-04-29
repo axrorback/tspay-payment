a7579feb6833422a63122d0754d13de176aeb802
axrorback <axrorback@gmail.com>
2026-04-29 09:55:38 +0500

    Add custom 404 error page
    - Defined a `custom_404` handler in `urls.py`.
    - Added a new template for 404 errors (`templates/errors/404.html`).

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path , include
+from django.shortcuts import render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('payment/', include('payment.urls')),
 ]
+def custom_404(request, exception):
+    return render(request, 'errors/404.html', status=404)
+
+handler404 = 'config.urls.custom_404'
